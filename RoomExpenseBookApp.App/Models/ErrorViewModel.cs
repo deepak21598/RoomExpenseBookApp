dcaf54e300edff764321f23dc6acfe6b7b5df959
Deepak <Deepak@DESKTOP-0R84T8E>
2021-10-16 13:54:51 +0530

    Add project files.

---
+++ RoomExpenseBookApp.App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RoomExpenseBookApp.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
